a05efbf3206c7bb417b63012c95c483a867ac954
ayesha <ayeshakhalil432@gmail.com>
2024-01-05 23:45:05 +0500

    Initializing first variable and displaying it

--- Basic-JavaScript-Practice/q1.js
+++ Basic-JavaScript-Practice/q1.js
@@ -1,1 +1,4 @@
 // Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+let a=2;
+console.log(a);
