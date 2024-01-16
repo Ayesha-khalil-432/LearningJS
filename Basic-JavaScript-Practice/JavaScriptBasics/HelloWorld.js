25d57ffbcffb11213181a72100f28b00c1471221
ayesha <ayeshakhalil432@gmail.com>
2024-01-16 22:16:24 +0500

    Basics in javaScript

---
+++ Basic-JavaScript-Practice/JavaScriptBasics/HelloWorld.js
@@ -0,0 +1,8 @@
+let js = 'amazing';
+// if (js === 'amazing') alert('JS is FUN!');
+
+console.log(40 + 8 + 23 - 10);
+
+console.log(js);
+let myName = 'Ayesha';
+console.log(myName);
--- Basic-JavaScript-Practice/q1.js
+++
@@ -1,4 +0,0 @@
-// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
-
-let a=2;
-console.log(a);
