db1803c1c25cd50ade7004d74afbfe9b3eccb17e
ayesha <ayeshakhalil432@gmail.com>
2024-01-27 21:55:29 +0500

    keeping record of high score

--- DOM-Manipulation/Guess-My-Number/script.js
+++ DOM-Manipulation/Guess-My-Number/script.js
@@ -52,10 +52,10 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector('.score').textContent = score;
-    document.querySelector('.message').textContent = 'Start guessing...';
-    document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.guess').value = '';
     document.querySelector('.number').textContent = '?';
     document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('body').style.backgroundColor = '#222';
 })
